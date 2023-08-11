ea5d554d6dfbf318c0d931f367104cd6bb1bc010
angelindisguise <76496683+angelindisguise@users.noreply.github.com>
2023-08-11 20:02:09 +0100

    hehe

--- main.cpp
+++ main.cpp
@@ -12,10 +12,17 @@ int main()
     int number2 = 017; //octal
     int number3 = 0x0f; //hexadecimal
     int number4 = 0b00001111; //binary
+    string name;
 
     cout << "number1: " << number1 << endl;
     cout << "number2: " << number2 << endl;
     cout << "number3: " << number3 << endl;
     cout << "number4: " << number4 << endl;
+
+    cout << "Whats your name? ";
+    //cin >> name;
+    getline (cin, name);
+    cout << "Hello " << name << ", welcome to my C++ github repo";
+    
     return 0;
 }
